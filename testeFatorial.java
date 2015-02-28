d3f718a300c473b5ffbacde1cb8c42053a8cb2ba
Raphael Fontaine <fontaine@gmail.com>
2015-02-28 15:27:28 -0300

    Alteracao para lancar excecao em caso de numeros negativos

--- testeFatorial.java
+++ testeFatorial.java
@@ -1,7 +1,7 @@
 class testeFatorial {
 	public static void main(String[] args) {
 		fatorial fat = new fatorial();
-		int resultado = fat.calcularFatorial(5);
+		int resultado = fat.calcularFatorial(-5);
 		System.out.println(resultado);
 	}
 }
