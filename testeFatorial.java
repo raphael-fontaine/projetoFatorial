9cdd8697e773aa5b7fcb30ce4a86370692c097a3
Raphael Fontaine <fontaine@gmail.com>
2015-03-07 13:22:07 -0300

    Calculo do fatorial de 10 e renomeacao da variavel local

--- testeFatorial.java
+++ testeFatorial.java
@@ -2,7 +2,7 @@ class testeFatorial {
 	// Comentario inserido
 	public static void main(String[] args) {
 		fatorial fat = new fatorial();
-		int resultado = fat.calcularFatorial(0);
-		System.out.println(resultado);
+		int r = fat.calcularFatorial(10);
+		System.out.println(r);
 	}
 }
